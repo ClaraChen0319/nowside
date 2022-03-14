c705dd00cd321882a1c68fa5ac8070c0ba0ffdf8
Seon Kuraito <seon.kuraito@gmail.com>
2022-03-14 23:19:59 +0800

    新增 layer components 樣式、修正畫面顯示

--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -1,29 +1,29 @@
 <script>
-  var toggleInputContainer = function (input) {
-      if (input.value != "") {
-          input.classList.add('filled');
-      } else {
-          input.classList.remove('filled');
-      }
-  }
+  // let toggleInputContainer = function (input) {
+  //     if (input.value != "") {
+  //         input.classList.add('filled');
+  //     } else {
+  //         input.classList.remove('filled');
+  //     }
+  // }
 
-  var labels = document.querySelectorAll('.label');
-  for (var i = 0; i < labels.length; i++) {
-      labels[i].addEventListener('click', function () {
-          this.previousElementSibling.focus();
-      });
-  }
+  // let labels = document.querySelectorAll('.label');
+  // for (let i = 0; i < labels.length; i++) {
+  //     labels[i].addEventListener('click', function () {
+  //         this.previousElementSibling.focus();
+  //     });
+  // }
 
-  window.addEventListener("load", function () {
-      var inputs = document.getElementsByClassName("input");
-      for (var i = 0; i < inputs.length; i++) {
-          console.log('looped');
-          inputs[i].addEventListener('keyup', function () {
-              toggleInputContainer(this);
-          });
-          toggleInputContainer(inputs[i]);
-      }
-  });
+  // window.addEventListener("load", function () {
+  //     let inputs = document.getElementsByClassName("input");
+  //     for (let i = 0; i < inputs.length; i++) {
+  //         console.log('looped');
+  //         inputs[i].addEventListener('keyup', function () {
+  //             toggleInputContainer(this);
+  //         });
+  //         toggleInputContainer(inputs[i]);
+  //     }
+  // });
 </script>
 
 <template>
@@ -31,39 +31,34 @@
     <img
       src="https://play.tailwindcss.com/img/beams.jpg"
       alt=""
-      class="absolute top-1/2 left-1/2 w-screen h-screen -translate-x-1/2 -translate-y-1/2 h-scerrn"
+      class="absolute top-1/2 left-1/2 w-screen h-screen -translate-x-1/2 -translate-y-1/2"
     >
-
-    <div class="relative px-6 pt-10 pb-8 bg-white ring-1 ring-gray-900/5 shadow-xl sm:px-10 sm:mx-auto sm:min-w-[416px] sm:rounded">
+    <div class="relative px-6 pt-10 pb-8 min-w-[416px] bg-white ring-1 ring-gray-300 shadow-xl sm:px-10 sm:mx-auto sm:rounded">
       <div class="mx-auto max-w-md">
-        <h1 class="flex justify-center text-2xl">
-          LOGO
-        </h1>
-
         <div class="divide-y divide-gray-300">
           <div class="py-8 space-y-6 text-base leading-7 text-gray-700">
             <div class="relative mb-8">
               <input
                 id="email"
-                class="p-3 pt-5 pb-2 w-full rounded border border-gray-700 focus:border-green-500 active:border-green-500 focus:outline-none active:outline-none appearance-none input focus"
+                class="nowside-input custom-input"
                 type="text"
                 autofocus
               >
               <label
                 for="email"
-                class="absolute pt-4 pl-3 -mt-2 mt-2 mb-0 text-base cursor-text label leading-tighter text-gray-400"
+                class="absolute nowside-label custom-label"
               >信箱</label>
             </div>
             <div class="relative mb-8">
               <input
                 id="password"
-                class="p-3 pt-5 pb-2 w-full rounded border border-gray-700 focus:border-green-500 active:border-green-500 focus:outline-none active:outline-none appearance-none input focus"
+                class="nowside-input custom-input"
                 type="password"
                 autofocus
               >
               <label
                 for="password"
-                class="absolute pt-4 pl-3 -mt-2 mt-2 mb-0 text-base cursor-text label leading-tighter text-gray-400"
+                class="absolute nowside-label custom-label"
               >密碼</label>
             </div>
             <div class="flex justify-between content-start">
@@ -83,12 +78,11 @@
               </div>
             </div>
           </div>
-
           <div class="pt-8 text-base font-semibold leading-7">
             <button class="py-4 px-6 mb-8 w-full text-white bg-blue-600 rounded">
               登入
             </button>
-            <button class="py-4 px-6 mb-8 w-full text-blue-600 bg-blue-100 rounded border border-[#89AECD]">
+            <button class="py-4 px-6 mb-8 w-full text-blue-600 bg-blue-100 rounded border border-blue-300">
               立即註冊
             </button>
             <p class="flex justify-center text-xs text-blue-600">
@@ -102,23 +96,23 @@
 </template>
 
 <style>
-  .input {
+  .custom-input {
     transition: border 0.2s ease-in-out;
     min-width: 280px
   }
 
-  .input:focus+.label,
-  .input:active+.label,
-  .input.filled+.label {
-    font-size: .75rem;
-    transition: all 0.2s ease-out;
-    top: -0.1rem;
-    color: #40C3A9;
-  }
-
-  .label {
+  .custom-label {
     transition: all 0.2s ease-out;
     top: 0.4rem;
     left: 0;
+  }
+
+  .custom-input:focus+.custom-label,
+  .custom-input:active+.custom-label,
+  .custom-input.filled+.custom-label {
+    font-size: .75rem;
+    transition: all 0.2s ease-out;
+    top: -0.5rem;
+    color: #40C3A9;
   }
 </style>
