d361bef3a90ae2350008788f36deaa2ee47fa542
Seon Kuraito <seon.kuraito@gmail.com>
2022-03-15 08:46:29 +0800

    修正 views 檔案、路由表

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -7,14 +7,13 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      // component: HomeView
-      component: () => import('../components/LoginModal.vue')
+      component: HomeView
     },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
-    },
+    // {
+    //   path: '/signup',
+    //   name: 'signup',
+    //   component: () => import('../views/SignupView.vue')
+    // },
   ]
 })
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <script setup>
-// import TheWelcome from '@/components/TheWelcome.vue'
 import LoginModal from '@/components/LoginModal.vue';
 </script>
 
 <template>
   <main>
-    <!-- <TheWelcome /> -->
     <LoginModal></LoginModal>
   </main>
 </template>
