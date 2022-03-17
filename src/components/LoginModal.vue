f4363eee175778721b800ef6d2358fec7ee1ee4a
Seon Kuraito <seon.kuraito@gmail.com>
2022-03-17 15:25:00 +0800

    修正 LoginModal 畫面

--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -10,39 +10,54 @@
 </script>
 
 <template>
-  <div class="flex overflow-hidden relative flex-col justify-center py-6 min-h-screen">
-    <div class="relative py-8 px-6 mx-auto min-w-[416px] bg-white rounded ring-1 ring-gray-300 shadow-xl">
-      <div class="mx-auto max-w-md">
-        <div class="divide-y divide-gray-300">
-          <div class="py-8 leading-7 text-gray-700">
-            <div class="relative mb-8">
-              <input
-                id="email"
-                v-model="email"
-                class="nowside-input custom-input"
-                :class="{ 'filled': email !== '', '': email === '' }"
-                type="text"
-                autofocus
-              >
-              <label
-                for="email"
-                class="absolute nowside-label custom-label"
-              >信箱</label>
-            </div>
-            <div class="relative mb-4">
-              <input
-                id="password"
-                v-model="password"
-                class="nowside-input custom-input"
-                :class="{ 'filled': password !== '', '': password === '' }"
-                type="password"
-                autofocus
-              >
-              <label
-                for="password"
-                class="absolute nowside-label custom-label"
-              >密碼</label>
-            </div>
+  <div class="flex overflow-hidden relative flex-col justify-center min-h-screen">
+    <div class="relative mx-auto min-w-[464px] bg-white rounded ring-1 ring-gray-300 shadow-xl">
+      <div class="mx-auto max-w-[384px] divide-y divide-gray-300">
+        <div class="my-8 text-gray-700">
+          <div class="mb-10">
+            <img
+              src="@/assets/logo.png"
+              alt="logo"
+              class="mx-auto max-h-[126px] align-middle"
+            >
+          </div>
+          <div class="mb-10">
+            <form
+              class=""
+              action=""
+              method="POST"
+            >
+              <div class="relative mb-10">
+                <input
+                  id="email"
+                  v-model="email"
+                  name="email" 
+                  type="text"
+                  class="peer nowside-input"
+                  placeholder="信箱"
+                >
+                <label
+                  for="email"
+                  class="nowside-inputLabel"
+                  :class="[ email ? 'filled' : '' ]"
+                >信箱</label>
+              </div>
+              <div class="relative mb-6">
+                <input
+                  id="password"
+                  v-model="password"
+                  type="password"
+                  name="password"     
+                  class="peer nowside-input"
+                  placeholder="密碼"
+                >
+                <label
+                  for="password"
+                  class="nowside-inputLabel"
+                  :class="[ email ? 'filled' : '' ]"
+                >密碼</label>
+              </div>
+            </form>
             <div class="flex justify-between">
               <div class="flex items-center">
                 <input
@@ -60,17 +75,23 @@
               </div>
             </div>
           </div>
-          <div class="pt-8">
-            <button class="py-4 px-6 mb-8 w-full text-lg font-bold text-white bg-blue-600 rounded">
-              登入
-            </button>
-            <button class="py-4 px-6 mb-8 w-full text-lg font-bold text-blue-600 bg-blue-100 rounded border border-blue-300">
-              立即註冊
-            </button>
-            <p class="flex justify-center text-xs text-blue-600">
-              沒有信箱？立即註冊
-            </p>
-          </div>
+        </div>
+        <div class="pt-8 mb-8">
+          <button
+            type="button"
+            class="block py-4 mb-8 w-full text-lg font-bold text-center text-white bg-blue-600 hover:bg-blue-500 rounded focus:outline-none cursor-pointer"
+          >
+            登入
+          </button>
+          <RouterLink
+            class="flex justify-center py-4 px-6 mb-8 w-full text-lg font-bold text-blue-600 bg-blue-100 rounded border border-blue-300"
+            to="/signup"
+          >
+            立即註冊
+          </RouterLink>
+          <p class="flex justify-center text-xs text-blue-600">
+            沒有信箱？立即註冊
+          </p>
         </div>
       </div>
     </div>
