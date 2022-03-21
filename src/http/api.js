9dc744e49780437847c541ee8ceb62ed9e9f20e4
Seon Kuraito <seon.kuraito@gmail.com>
2022-03-21 16:43:36 +0800

    新增 /Signup API（待修正）

--- src/http/api.js
+++ src/http/api.js
@@ -2,4 +2,9 @@ import axiosInstance from './index';
 
 // 請求接口封裝
 export const login = (params) => axiosInstance.post('/Login',params);
-export const signup = (params) => axiosInstance.post('/SignUp',params);
+export const signup = (params) => axiosInstance.post('/SignUp',{
+    method: "post",
+    url: "http://sideprojectnow.rocket-coding.com/SignUp",
+    data: params,
+    headers: { "Content-Type": "multipart/form-data" },
+});
