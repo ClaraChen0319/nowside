92691e74b46c793741ac45178fe01a6db95066ae
Seon Kuraito <seon.kuraito@gmail.com>
2022-03-28 00:18:38 +0800

    修正 Footer.vue 字重

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,8 +10,8 @@ export default {
 </script>
 
 <template>
-  <section class="flex justify-center items-center w-full h-[130px] text-white bg-C_blue-700 dark:bg-C_blue-900">
-    © 2022 Side Project Now. All rights reserved.
+  <section class="flex justify-center items-center w-full h-[130px] bg-C_blue-700 dark:bg-C_blue-900">
+    <span class="font-medium text-white">© 2022 Side Project Now. All rights reserved.</span>
   </section>
 </template>
 
